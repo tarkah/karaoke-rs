fb26bdd9793282d0f4a16e30141552223e6068da
Cory Forsstrom <cforsstrom18@gmail.com>
2019-12-17 19:20:03 -0800

    fix clippy lint

--- src/collection.rs
+++ src/collection.rs
@@ -34,7 +34,7 @@ pub trait Custom {
 impl Custom for CollectionDB {
     //If file doesn't exist, create default. Load db from file.
     fn initialize(path: &PathBuf) -> Result<Box<CollectionDB>, failure::Error> {
-        let mut db: CollectionDB;
+        let db: CollectionDB;
 
         let mut db_path = path.to_path_buf();
         db_path.push("db.yaml");
